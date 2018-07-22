30debdf2e960119e69a78efbc53f1cf382777db4
yaoxiaoyu <yaoxiao0525@foxmail.com>
2018-07-22 19:13:16 +0800

    点击添加后添加元素

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("button").after("add")
+    })
+}   
+)
+
+// $(function() {
+//     $( "#dialog" ).dialog({
+//         modal: true,
+//         buttons: {
+//             文本: function() {
+//                 $( this ).after( "wenben" );
+//             },
+//             日期:function(){
+//                 $(this).after("riqi")
+//             }
+//         }
+//     });
+// });
