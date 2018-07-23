571a8242fb6f988494722cd8dfd473c30210816a
yaoxiaoyu <yaoxiao0525@foxmail.com>
2018-07-23 23:00:45 +0800

    完成添加字段和选择字段

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,27 @@
 
 
 $(document).ready(function(){
-    $("button").click(function(){
+    $("#add").click(function(){
         $("button").after(function() {
             $( "#dialog" ).dialog({
                 modal: true,
+                onLoad: function(){
+                    $('')
+                },
                 buttons: {
-                    文本: function() {
-                        $( '#add' ).after( "wenben" );
+                    提交: function() {
+                        if($("input[name='add']:checked").val()=='文本'){
+                            $( '#add' ).after( "<div><input id='inputtext'></input><button>删除</button></div>" );
+                        }else if($("input[name=add]:checked").val()=='日期'){
+                            $( '#add' ).after( "<div><input id='dateselector' placeholder='请选择日期'></input><button>删除</button></div>" );
+                            $( '#dateselector' ).after(function(){
+                                $('#dateselector').datepicker();
+                            });
+                        }
+                        $(this).dialog("close")
                     },
-                    日期:function(){
-                        $("#add").after("riqi")
+                    关闭:function(){
+                        $(this).dialog("close")
                     }
                 }
             });
