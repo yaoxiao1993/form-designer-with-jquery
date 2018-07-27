8e1b0a491c4e509b07c17c6165aa2c75b65f50af
yaoxiaoyu <yaoxiao0525@foxmail.com>
2018-07-27 22:40:50 +0800

    bug解决

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,20 @@
 
 
 $(document).ready(function(){
+
     $("#add").click(function(){
         $("button").after(function() {
             $( "#dialog" ).dialog({
                 modal: true,
-                onLoad: function(){
-                    $('')
-                },
                 buttons: {
                     提交: function() {
                         if($("input[name='add']:checked").val()=='文本'){
-                            $( '#preview' ).after( "<div ><input id='inputtext'></input><button id='delete'>删除</button></div>" );
+                            $( '#preview' ).after( "<div ><input class='inputtext'></input><button class='delete'>删除</button></div>" );
                             bindListener();
                         }else if($("input[name=add]:checked").val()=='日期'){
-                            $( '#preview' ).after( "<div><input id='dateselector' placeholder='请选择日期'></input><button id='delete'>删除</button></div>" );
-                            $( '#dateselector' ).after(function(){
-                                $('#dateselector').datepicker();
+                            $( '#preview' ).after( "<div><input class='dateselector' placeholder='请选择日期'></input><button class='delete'>删除</button></div>");
+                            $( '.dateselector' ).after(function(){
+                                $('.dateselector').datepicker();
                             });
                             bindListener();
                         }
@@ -28,20 +26,28 @@ $(document).ready(function(){
                 }
             });
         })
+    })
+
+    $("#preview").click(function(){
+        if($("#preview").text()=='预览'){
+            $("#add").hide();
+            if($(".delete").length>0){
+                $(".delete").hide();
+            }
+            $(this).text('编辑');
+            $(".inputtext").attr("disabled","disabled");
+        }else if($("#preview").text()=='编辑'){
+            $("#add").show();
+            $(".delete").show();
+            $(this).text('预览');
+            $(".inputtext").removeAttr("disabled");
+        }
+    })
+
         function bindListener(){
-            $("#delete").unbind().click(function(){
+            $(".delete").unbind().click(function(){
                 $(this).parent().remove()
             })
         }
-        $("#preview").click(function(){
-            $("#add").hide();
-            if($("#delete").length>0){
-                $("#delete").hide();
-            }
-            $(this).text('编辑');
-        })
-        
+
     })
-})
-
-
